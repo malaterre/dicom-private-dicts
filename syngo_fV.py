fb76e4906e63c7a0074536c73e698bd48a1fcfa7
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2015-11-30 12:16:55 +0100

    Fix parsing of public elements

--- syngo_fV.py
+++ syngo_fV.py
@@ -3,34 +3,57 @@
 
 import sys
 
-def print_element( group, element, creator, name ):
-  if creator: print '(%s,%s,"%s") %s'%(group,element,creator,name)
-  else: print "(%s,%s) %s"%(group,element,name)
+def print_element1( group, element, name, ret ):
+  if ret: print '(%s,%s) "%s" (RET)'%(group,element,name)
+  else: print '(%s,%s) "%s"'%(group,element,name)
 
-def process_public( line ):
-  name, group, element, vr, vm, ret = line.split('#')
-  creator = None
-  print_element( group, element, creator, name )
+def print_element2( group, element, creator, name, ret ):
+  if ret: print '(%s,%s,"%s") "%s" (RET)'%(group,element,creator,name)
+  else: print '(%s,%s,"%s") "%s"'%(group,element,creator,name)
 
-ret = "RETIRED"
+def process1( line ):
+  if line[0] != ';': # discard comment
+    group, element, name, vr, vm, sret = line.split('#')
+    ret = False
+    if sret == 'RET': ret = True
+    print_element1( group, element, name, ret )
 
-def process_private( line ):
-  v = line.split(',')
-  if len(v) == 5:
-    name, group, element, vr, vm = line.split(',')
-    creator = None
-  elif len(v) == 6:
-    name, group, creator, element, vr, vm = line.split(',')
-    if ret in name: name = name[:-8]
-  else:
-    assert None
-  print_element( group, element, creator, name )
+retired = "RETIRED"
+
+def process2( line ):
+  name, group, element, vr, vm = line.split(',')
+  ret = False
+  if retired in name:
+    name = name[:-8]
+    ret = True
+  print_element1( group, element, name, ret )
+
+def process3( line ):
+  name, group, creator, element, vr, vm = line.split(',')
+  ret = False
+  if retired in name:
+    name = name[:-8]
+    ret = True
+  print_element2( group, element, creator, name, ret )
+
+def process( mode, line ):
+  if mode == 0:
+    process1( line )
+  elif mode == 1:
+    process2( line )
+  elif mode == 2:
+    process3( line )
 
 if __name__ == "__main__":
-  filename= sys.argv[1]
+  filename = sys.argv[1]
+  parse = 0
   with open(filename,'rU') as f:
     for line in f:
       line = line.rstrip('\n')
-      if line and line[0] != ';': # discard comment
-        if '#' in line: process_public( line )
-        else: process_private( line )
+      if line:
+        if ord(line[0]) == 0:
+          parse = parse + 1
+          #print line, ord(line[0])
+          line = line.lstrip(chr(0))
+          #print line, ord(line[0])
+        process( parse, line )
