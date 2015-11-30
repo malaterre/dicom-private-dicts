9bec4fa334b6c4e0cdf23c3173832a9b3176fc34
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2015-11-30 14:51:01 +0100

    Minor tweaks

--- syngo_dict.sh
+++ syngo_dict.sh
@@ -12,6 +12,19 @@ unzip -d tmp "$zipfile"
 SKIP=1477268
 COUNT=185436
 
-filename=tmp/syngo_fV/syngo_fV.exe
+infile=tmp/syngo_fV/syngo_fV.exe
+tmpfile=syngo_tmp.txt
+outfile=syngo_fV.txt
 
-dd skip=$SKIP count=$COUNT if="$filename" of=syngo_fV.txt bs=1
+# extract all 3 dicts at once, they will be separated by 2 or 3 '0A' (end of file) character.
+dd skip=$SKIP count=$COUNT if="$infile" of="$tmpfile" bs=1
+
+# curly single quotes (U+2018 and U+2019, which are 0x91 and 0x92 in CP1252
+# (also known as MS-ANSI and WINDOWS-1252; a common 8-bit encoding on Windows)).
+iconv -o "$outfile" -f WINDOWS-1252 -t UTF8 $tmpfile
+
+# Prefer UNIX eol:
+sed -i 's/\r//' "$outfile"
+
+# fix end of file:
+echo "" >> "$outfile"
