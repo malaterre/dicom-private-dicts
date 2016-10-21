8ca4b700cd1cf3e3141ee4e0a4b0fb73306c12a2
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2016-10-21 13:59:38 +0200

    add opt

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ wget -c $URL
 
 PAGES=58,64
 
-java -jar $TABULA --silent -f JSON -a 234.647,38.317,794.898,578.479 -p 57 -o data1.json poc_013216.pdf
+java -jar $TABULA --silent --spreadsheet -f JSON -a 234.647,38.317,794.898,578.479 -p 57 -o data1.json poc_013216.pdf
 java -jar $TABULA --silent --spreadsheet -f JSON -p $PAGES -o data2.json poc_013216.pdf
-java -jar $TABULA --silent -f JSON -a 54.686,44.269,123.88,568.807 -p 65 -o data3.json poc_013216.pdf
+java -jar $TABULA --silent --spreadsheet -f JSON -a 54.686,44.269,123.88,568.807 -p 65 -o data3.json poc_013216.pdf
