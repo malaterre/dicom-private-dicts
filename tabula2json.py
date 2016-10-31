9904506fbb2376e87b3c022f10fef7c1dee2b317
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2016-10-31 11:55:50 +0100

    update

--- tabula2json.py
+++ tabula2json.py
@@ -8,44 +8,41 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--files', help='dir help')
 parser.add_argument('--output', help='dir help')
+parser.add_argument('--use_table_header', help='dir help')
 args = parser.parse_args()
 
-#files = [ 'data1.json', 'data2.json', 'data3.json']
-#files = [ '/tmp/foo.json']
+# we are given a list of files that contains tables to be merged in single dict
 files = args.files.split(',')
+
+def normalize_header( header ):
+  ret = [None] * len(header)
+  for index,it in enumerate(header):
+    txt = it['text']
+    if txt == 'Attribute Name':
+      ret[ index ] = u'AttributeName'
+    else:
+      ret[ index ] = txt
+  return ret
 
 d=[]
 for f in files:
   with open(f) as data_file:    
-      pages = json.load(data_file)
-  
-  #pprint(pages)
-  #print data
-  #for i in data:
-  #  print i
-  #  print "\n"
-  
-  for page in pages:
-    data = page['data']
-    #print data.__class__
-    #print "header:", data[0]
-    k = data[0]
-    #print "header:",k[0]['text'],k[1]['text'],k[2]['text'],k[3]['text']
-    for j in data[1:]:
-      #for k in j:
-      #print k['text']
-      #print j[0]['text'],j[1]['text'],j[2]['text'],j[3]['text'].replace('\r',' ')
-      elem={}
-      elem[ k[0]['text'] ] = j[0]['text']
-      elem[ k[1]['text'] ] = j[1]['text']
-      elem[ k[2]['text'] ] = j[2]['text']
-      elem[ k[3]['text'] ] = j[3]['text'].replace('\r',' ')
-      d.append(elem)
-  #    for attribute, value in j.iteritems():
-  # "      print attribute
-  #        #print attribute, value # example usage
-  
-  #print d
+    pages = json.load(data_file)
+    for page in pages:
+      data = page['data']
+      numcol = len( data[0] )
+      k = normalize_header( data[0] )
+      for j in data[1:]:
+        elem={}
+        for index,col in enumerate(k):
+          elem[ col ] = j[index]['text'].replace('\r',' ')
+        #elem[ k[0] ] = j[0]['text']
+        #elem[ k[1] ] = j[1]['text']
+        #elem[ k[2] ] = j[2]['text']
+        #elem[ k[3] ] = j[3]['text'].replace('\r',' ')
+        d.append(elem)
+
+# now that dict is complete, save as json:
 ojson = args.output
 with open(ojson,'w') as out_file:
   out_file.write( json.dumps(d, sort_keys=True, indent=4) )
