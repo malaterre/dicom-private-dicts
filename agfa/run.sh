5b26bb94e890d490dd276105be9979a8a71671ba
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2016-11-01 11:30:40 +0100

    fix

--- agfa/run.sh
+++ agfa/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 set -x
-./tabula.sh -o "agfa/table0.json" -p 15 -a 400,173,650,1000 "agfa/000978_tcm583-21764.pdf"
-./tabula2xml.py  --header "AttributeName,Tag,VR,DefaultValue"  --owner "AGFA"  --files "agfa/table0.json" --output "agfa/000978_tcm583-21764_0.xml"
-./tabula.sh -o "agfa/table0.json" -p 25 -a 50,61,400,1000 "agfa/000946_tcm583-21755.pdf"
-./tabula2xml.py  --header "AttributeName,Tag,VR,Type,DefaultValue,UNK"  --owner "AGFA_ADC_Compact"  --files "agfa/table0.json" --output "agfa/000946_tcm583-21755_0.xml"
-./tabula.sh -o "agfa/table0.json" -p 16 -a 100,50,250,1000 "agfa/000946_tcm583-21755.pdf"
-./tabula2xml.py  --header "AttributeName,Tag,DefaultValue"  --owner "AGFA"  --files "agfa/table0.json" --output "agfa/000946_tcm583-21755_1.xml"
+./tabula.sh -o "agfa/chunk0.json" -p 15 -a 400,173,650,1000 "agfa/000978_tcm583-21764.pdf"
+./tabula2xml.py  --header "AttributeName,Tag,VR,DefaultValue"  --owner "AGFA"  --files "agfa/chunk0.json" --output "agfa/000978_tcm583-21764_0.xml"
+./tabula.sh -o "agfa/chunk0.json" -p 25 -a 50,61,400,1000 "agfa/000946_tcm583-21755.pdf"
+./tabula2xml.py  --header "AttributeName,Tag,VR,Type,DefaultValue,UNK"  --owner "AGFA_ADC_Compact"  --files "agfa/chunk0.json" --output "agfa/000946_tcm583-21755_0.xml"
+./tabula.sh -o "agfa/chunk0.json" -p 16 -a 100,50,250,1000 "agfa/000946_tcm583-21755.pdf"
+./tabula2xml.py  --header "AttributeName,Tag,DefaultValue"  --owner "AGFA"  --files "agfa/chunk0.json" --output "agfa/000946_tcm583-21755_1.xml"
--- process_json.py
+++ process_json.py
@@ -25,41 +25,55 @@ with open(f) as data_file:
         out_file2.write( '#!/bin/sh\n' )
         out_file2.write( 'set -e\n' )
         out_file2.write( 'set -x\n' )
+        # let's write the series of files for this manufacturer in an index file
         out_file3.write( '<index>\n' )
         for it in data:
           url = it['url']
           out_file.write( url )
           out_file.write( '\n' )
           tables = it['tables']
-          for indexold, table in enumerate(tables):
+          # loop over each table from this PDF doc:
+          # a table has only a single owner for now
+          for indext, table in enumerate(tables):
             files=[]
             chunks = table['chunks']
+            # a table is split into chunks (basically page or pages-range)
             for index, chunk in enumerate(chunks):
-              fil = "%s/table%d.json" % (dirpath,index)
+              fil = "%s/chunk%d.json" % (dirpath,index)
               files.append( fil )
+              # use tabula to extra a single chunk:
               out_file2.write( './tabula.sh -o "%s" ' % fil )
+              # Define as page range (whole page):
               if chunk.has_key('pages'):
                 out_file2.write( "-p " )
                 out_file2.write( chunk['pages'] )
+              # or a single page (with an area)
               elif chunk.has_key('page'):
                 out_file2.write( "-p " )
                 out_file2.write( str(chunk['page']) )
                 if chunk.has_key('area'):
                   out_file2.write( " -a " )
                   out_file2.write( chunk['area'] )
+              else:
+                assert(False)
+              # compute the PDF local path:
               inpdf = "%s/%s" % (dirpath, os.path.basename( it['url']))
               filename, file_extension = os.path.splitext(inpdf)
-              outxml = "%s_%d.xml" % (filename, indexold)
+              # compute the target XML file (one per table)
+              outxml = "%s_%d.xml" % (filename, indext)
               out_file2.write( ' "%s"' % inpdf )
               out_file2.write( '\n' )
-            # now convert:
+            # now convert the generated JSON into proper XML
             fstr = ','.join(files)
             opts = ""
+            # pass the table header (defined manually, generally because JSON output is bogus)
             if table.has_key( "header" ):
               assert( not table.has_key( "opts" ) )
               opts += ' --header "%s" ' % ",".join(table['header'])
+            # pass the table owner
             if table.has_key( "owner" ):
               opts += ' --owner "%s" ' % table['owner']
+            # special option to use the table header directly from the JSON generated output
             if table.has_key( "opts" ):
               opts += table['opts']
             out_file2.write( './tabula2xml.py %s --files "%s" --output "%s"\n' % (opts, fstr, outxml) )
