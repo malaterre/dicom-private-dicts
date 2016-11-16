ddf3ffeb4663424a1c5d34704bf02ded2de9ce1c
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2016-11-16 14:12:30 +0100

    add run2

---
+++ agfa/run2.sh
@@ -0,0 +1,4 @@
+./tabula2xml.py  --header "AttributeName,Tag,VR,DefaultValue"  --owner "AGFA"  --files "agfa/258e6887d6b66b92d44d50a7cf4d8a80_0_0.json" --output "agfa/000978_tcm583-21764_0.xml"
+./tabula2xml.py  --header "AttributeName,Tag,VR,Type,DefaultValue,UNK"  --owner "AGFA_ADC_Compact"  --files "agfa/ad48c1a03a1e2b6c7e8126c6e42d7870_0_0.json" --output "agfa/000946_tcm583-21755_0.xml"
+./tabula2xml.py  --header "AttributeName,Tag,DefaultValue"  --owner "AGFA"  --files "agfa/ad48c1a03a1e2b6c7e8126c6e42d7870_1_0.json" --output "agfa/000946_tcm583-21755_1.xml"
+./tabula2xml.py  --header "Tag,VR,VM,AttributeName,Definition"  --owner "1.2.840.113681"  --files "agfa/2e52b0254d8591c9839d1f855f08ee35_0_0.json" --output "agfa/000410_tcm583-21790_0.xml"
