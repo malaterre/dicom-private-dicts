2045b85ea4115201349c5afa08aa4f821117fed4
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2016-11-22 08:48:33 +0100

    some progress

--- re/pms/dump1.py
+++ re/pms/dump1.py
@@ -38,10 +38,56 @@ def doit2(i,f):
   array[i]['value']=s[0]
   array[i]['len']=l0[0]
 
+FixPadding = [
+ { 'pad' : 1 },
+ { 'pad' : 1 },
+ { 'pad' : 1 },
+ { 'pad' : 2 },
+ { 'pad' : 0 },
+ { 'pad' : 6 },
+ { 'pad' : 2 },
+ { 'pad' : 1 },
+ { 'pad' : 7 },
+ { 'pad' : 4 },
+ { 'pad' : 4 },
+ { 'pad' : 3 },
+]
+AddPadding = False
+
+def doit3(i,f):
+  global AddPadding
+  """
+  chunk = f.read(0x1)
+  if AddPadding:
+    chunk = f.read(0x1)
+  """
+  pad = FixPadding[i]['pad']
+  chunk = f.read(pad)
+  #z = unpack('<B', chunk)
+  #print hex(z[0]),z[0]
+  #if z[0] != 0:
+  #  f.seek(-1,1)
+  chunk = f.read(0x2)
+  d = unpack('<H', chunk)
+  fl = d[0]
+  #print pad,hex(f.tell()),hex(fl),fl
+  if fl % 2 == 0:
+    AddPadding = True
+  else:
+    AddPadding = False
+  chunk = f.read(0x2)
+  d = unpack('<H', chunk)
+  #print hex(d[0])
+  assert d[0] == 0
+  chunk = f.read(fl)
+  with open("output%d.dad" % i, "w") as text_file:
+    text_file.write( chunk[:-2] )
+
 if __name__ == "__main__":
   filename = sys.argv[1]
   with open(filename,'rb') as f:
-    f.seek( 104932524 )
+    f.seek( 104932524 ) # 0x64124ac
+    # file type 1:
     chunk = f.read(0x2)
     d = unpack('>H', chunk)
     #print d
@@ -58,6 +104,44 @@ if __name__ == "__main__":
     assert z[0] == 0
     for i in range(0,60):
       doit2(i,f)
+    # file type 2:
+    chunk = f.read(0x4)
+    z = unpack('<I', chunk)
+    #print "debug: %d" % z
+    assert z[0] == 0
+    for i in range(0,12):
+      doit3(i,f)
+    #print hex(f.tell())
+    chunk = f.read(0x4)
+    z = unpack('<I', chunk)
+    assert z[0] == 0
+    chunk = f.read(0x4)
+    z = unpack('<I', chunk)
+    #print hex(z[0])
+    fl = z[0]
+    chunk = f.read(fl)
+    #print chunk
+    #print hex(f.tell())
+    chunk = f.read(0x3)
+    chunk = f.read(0x4)
+    chunk = f.read(0x4)
+    z = unpack('<I', chunk)
+    #print hex(z[0])
+    fl = z[0]
+    chunk = f.read(fl)
+    #print chunk[:-1]
+    #print hex(f.tell())
+    chunk = f.read(0x3)
+    chunk = f.read(0x4)
+    z = unpack('<I', chunk)
+    #print hex(z[0])
+    chunk = f.read(fl+4)
+    #print chunk
+    fixlen = 0xb02
+    chunk = f.read(fixlen)
+    #print chunk
+    print hex(f.tell())
+
   #print array
-  print json.dumps(array, sort_keys=True, indent=4)
+  #print json.dumps(array, sort_keys=True, indent=4)
   
